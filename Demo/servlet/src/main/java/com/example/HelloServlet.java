f0ba843b1a78ff3089445e5a08b2e9ed52e653ef
george-ball <george@jgserv.co.uk>
2026-05-05 13:31:06 +0100

    Servlet Demo

---
+++ Demo/servlet/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().write("Hello, World\n");
+  }
+}
