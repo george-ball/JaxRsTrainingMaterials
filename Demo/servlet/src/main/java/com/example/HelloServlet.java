e21c2a63c64041ae869d3254197c1c22ce05712f
george-ball <george@jgserv.co.uk>
2026-05-06 10:29:07 +0100

    more xml docs

--- Demo/servlet/src/main/java/com/example/HelloServlet.java
+++ Demo/servlet/src/main/java/com/example/HelloServlet.java
@@ -12,6 +12,6 @@ public class HelloServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().write("Hello, World\n");
+    resp.getWriter().write("Hello, Everyone\n");
   }
 }
